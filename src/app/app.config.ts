2c94117c31ec064bb115fc7186d39a9e2d854b32
QimbE <58182585+QimbE@users.noreply.github.com>
2023-12-09 17:10:36 +0500

    graphql

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,16 @@ import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import {provideRouter, RouterModule} from '@angular/router';
 
 import { routes } from './app.routes';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient} from "@angular/common/http";
 import {AuthenticationInterceptor} from "./services/authentication-interceptor.service";
 import {AutologoutInterceptor} from "./services/autologout-interceptor.service";
+import { graphqlProvider } from './graphql.provider';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
     {provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: AutologoutInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: AutologoutInterceptor, multi: true}, provideHttpClient(), graphqlProvider
   ]
 };
